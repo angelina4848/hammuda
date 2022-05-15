5bef2e06522701b1cb78d87ea6f8ed0ba1c1da09
Anna Galytska <anna@Annas-MacBook-Pro.local>
2022-05-15 16:39:30 +0200

    encoder and display added

--- sketch_may12a/sketch_may12a.ino
+++ sketch_may12a/sketch_may12a.ino
@@ -1,7 +1,21 @@
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27,16,2); 
+
+
+#define CLK 10
+#define DT 9
+#define SW 8
+
+#include "GyverEncoder.h"
+#include "TimerOne.h"
+Encoder enc1(CLK, DT, SW);  
 
 #define rain_sensor 5
 
 #define moisture_limit 800
+
+
+
 
 #define r1 2
 #define r2 3
@@ -20,7 +34,10 @@ int s2;
 int s3;
 int s4;
 
+int temp;
+int hum;
 
+int page = 1;
 void setup() {
   // declaración de reles
 pinMode(r1, OUTPUT);
@@ -28,11 +45,34 @@ pinMode(r2, OUTPUT);
 pinMode(r3, OUTPUT);
 pinMode(r4, OUTPUT);
 
+enc1.setType(TYPE2);
+
 Serial.begin(9600);
-
+  // Inicializar el LCD
+  lcd.init();
+  
+  //Encender la luz de fondo.
+  lcd.backlight();
+  
+  // Escribimos el Mensaje en el LCD.
+  lcd.print("t(C)=    h(%)=");
+  Timer1.initialize(1000);            // установка таймера на каждые 1000 микросекунд (= 1 мс)
+  Timer1.attachInterrupt(timerIsr);   // запуск таймера
 }
-
+void timerIsr() {   // прерывание таймера
+  enc1.tick();     // отработка теперь находится здесь
+}
 void loop() {
+  
+
+   
+
+  
+  lcd.setCursor(5, 0);
+  lcd.print(temp);
+  lcd.setCursor(14, 0);
+  lcd.print(hum);
+
   
   s1 = analogRead(A0);
   s2 = analogRead(A1);
@@ -52,4 +92,44 @@ if(s3 >= moisture_limit){
 if(s4 >= moisture_limit){
   digitalWrite(r4, 1);
 }
+
+if(enc1.isRight()){
+  page++;
+if(page == 5){
+  page = 1;
+}
+}
+if(enc1.isLeft()){
+  page--;
+  if(page == 0){
+  page = 4;
+}
+}
+
+switch(page){
+  case 1:
+  lcd.setCursor(0,1);
+  lcd.print("humedad1: ");
+  
+  lcd.print(s1);
+  break;
+    case 2:
+    lcd.setCursor(0,1);
+  lcd.print("humedad2: ");
+  lcd.print(s2);
+  break;
+    case 3:
+    lcd.setCursor(0,1);
+  lcd.print("humedad3: ");
+  lcd.print(s3);
+  break;
+    case 4:
+    lcd.setCursor(0,1);
+
+  lcd.print("humedad4: ");
+  lcd.print(s4);
+  break;
+  
+}
+Serial.println(page);
 }
